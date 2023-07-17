d538d2c97cab56af77408467dc5154396f2a03f3
Zain Zafar <zainzafar@Zains-MacBook-Pro.local>
2023-07-17 09:52:29 +0100

    router updated for gh-pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { routes } from "./routes";
 
-export default createBrowserRouter(routes);
+export default createHashRouter(routes);
